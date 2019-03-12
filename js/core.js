d25a30ddccb2afb6d86f71605a7eebb987520d6d
Vladyslav Syzov <vsyz@telenor.dk>
2019-03-12 09:42:13 +0200

    add result verification

--- js/core.js
+++ js/core.js
@@ -1,118 +1,103 @@
 function checkResult() {
 
-		var cellValue = 1;
-		var result = true;
-		
-		for(var x = 0; i < 4; x++){
-			
-			for(var y = 0; y < 4; y++){
-			
-				var selectors = '[data-x="{x}"][data-y="{y}"]'
-					.replace("{x}", x)
-					.replace("{y}", y);
-				var cell = document.querySelector(selectors);
+	var result = "";
 
-				result = result && cell.innerHTML == cellValue;
-				
-				if(!result){
-					return false;
-				}
-				
-				cellValue++;
-			}
-			
+	for (var y = 0; y < 4; y++) {
+
+		for (var x = 0; x < 4; x++) {
+
+			var selectors = '[data-x="{x}"][data-y="{y}"]'
+				.replace("{x}", x)
+				.replace("{y}", y);
+			var cell = document.querySelector(selectors);
+
+			result += cell.innerHTML + ',';
 		}
-		
-		return result;
 
 	}
-	
-	var emptyPosition = {
-		x: 3,
-		y: 3
+
+	return result === "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,,";
+}
+
+var emptyPosition = {
+	x: 3,
+	y: 3
+};
+
+function onCellClick() {
+
+	if (Math.abs(this.dataset.x - emptyPosition.x) + Math.abs(this.dataset.y - emptyPosition.y) !== 1) {
+		return;
 	}
 
-	var cells = document.getElementsByClassName('cell');
+	var selectors = '[data-x="{x}"][data-y="{y}"]'
+		.replace("{x}", emptyPosition.x)
+		.replace("{y}", emptyPosition.y);
 
-	function onCellClick() {
+	var emptyCellDiv = document.querySelector(selectors);
 
-		if (Math.abs(this.dataset.x - emptyPosition.x) + Math.abs(this.dataset.y - emptyPosition.y) !== 1) {
-			return;
-		}
+	emptyCellDiv.innerHTML = this.innerHTML;
+	this.innerHTML = '';
+
+	emptyPosition.x = this.dataset.x;
+	emptyPosition.y = this.dataset.y;
+
+	if (checkResult()) {
+		alert('You win!');
+	}
+};
+
+var initialDataCells = [
+	{ x: 0, y: 0 },
+	{ x: 1, y: 0 },
+	{ x: 2, y: 0 },
+	{ x: 3, y: 0 },
+	{ x: 0, y: 1 },
+	{ x: 1, y: 1 },
+	{ x: 2, y: 1 },
+	{ x: 3, y: 1 },
+	{ x: 0, y: 2 },
+	{ x: 1, y: 2 },
+	{ x: 2, y: 2 },
+	{ x: 3, y: 2 },
+	{ x: 0, y: 3 },
+	{ x: 1, y: 3 },
+	{ x: 2, y: 3 },
+	{ x: 3, y: 3 }
+];
+
+function refresh() {
+	var dataCells = initialDataCells.slice();
+	for (var i = 0; i < 16; i++) {
+
+		var index = Math.floor(dataCells.length * Math.random());
+
+		var cellItem = dataCells[index];
 
 		var selectors = '[data-x="{x}"][data-y="{y}"]'
-			.replace("{x}", emptyPosition.x)
-			.replace("{y}", emptyPosition.y);
+			.replace("{x}", cellItem.x)
+			.replace("{y}", cellItem.y);
 
 		var emptyCellDiv = document.querySelector(selectors);
 
-		emptyCellDiv.innerHTML = this.innerHTML;
-		this.innerHTML = '';
+		if (i === 0) {
+			emptyCellDiv.innerHTML = '';
+			emptyPosition = cellItem;
+		} else {
+			emptyCellDiv.innerHTML = i;
+		}
 
-		emptyPosition.x = this.dataset.x;
-		emptyPosition.y = this.dataset.y;
-	};
+		dataCells.splice(index, 1);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+
+	var cells = document.getElementsByClassName('cell');
 
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].addEventListener('click', onCellClick);
 	}
-
-	var initialDataCells = [
-		{ x: 0, y: 0 },
-		{ x: 1, y: 0 },
-		{ x: 2, y: 0 },
-		{ x: 3, y: 0 },
-		{ x: 0, y: 1 },
-		{ x: 1, y: 1 },
-		{ x: 2, y: 1 },
-		{ x: 3, y: 1 },
-		{ x: 0, y: 2 },
-		{ x: 1, y: 2 },
-		{ x: 2, y: 2 },
-		{ x: 3, y: 2 },
-		{ x: 0, y: 3 },
-		{ x: 1, y: 3 },
-		{ x: 2, y: 3 },
-		{ x: 3, y: 3 }
-	];
-
-	function refresh() {
-
-		var dataCells = initialDataCells.slice();
-		for (var i = 0; i < 16; i++) {
-
-			var index = Math.floor(dataCells.length * Math.random());
-
-			var cellItem = dataCells[index];
-
-			var selectors = '[data-x="{x}"][data-y="{y}"]'
-				.replace("{x}", cellItem.x)
-				.replace("{y}", cellItem.y);
-
-			var emptyCellDiv = document.querySelector(selectors);
-
-			if (i === 0) {
-				emptyCellDiv.innerHTML = '';
-				emptyPosition = cellItem;
-			} else {
-				emptyCellDiv.innerHTML = i;
-			}
-
-			dataCells.splice(index, 1);
-		}
-
-		//for (var i = 0; i < 4; i++) {
-
-		//	for (var j = 0; j < 4; j++) {
-		//		var selectors = '[data-x="{x}"][data-y="{y}"]'
-		//			.replace("{x}", i)
-		//			.replace("{y}", j);
-
-		//		var emptyCellDiv = document.querySelector(selectors);
-		//		emptyCellDiv.innerHTML = this.innerHTML;
-		//	}
-		//}
-
-	}
-
-	refresh();
+}
